e161d72843d9576fa3a9be9b7a9e3f6df2269128
Oleksandr Tsymbal <oleksandrtsymbal@medakadem.com>
2024-04-28 15:32:39 +0300

    #14 added support for Serilog.Settings.Configurations, updated README

--- src/Serilog.Sinks.Loki/LokiLabel.cs
+++ src/Serilog.Sinks.Loki/LokiLabel.cs
@@ -4,7 +4,7 @@
     /// Describes global loki label.
     /// All <see cref="LokiLabel"/> will added as label to each log event
     /// </summary>
-    public readonly struct LokiLabel : IEqualityComparer<LokiLabel>, IEquatable<LokiLabel>
+    public class LokiLabel : IEqualityComparer<LokiLabel>, IEquatable<LokiLabel>
     {
         /// <summary>
         /// Key of label
@@ -16,7 +16,6 @@
         /// </summary>
         public string Value { get; }
 
-
         /// <summary>
         /// Creates new instance of <see cref="LokiLabel"/>
         /// </summary>
@@ -27,37 +26,54 @@
             Key = key;
             Value = value;
         }
+
         /// <inheritdoc/>
-        public bool Equals(LokiLabel x, LokiLabel y)
+        public bool Equals(LokiLabel? x, LokiLabel? y)
         {
-            return x.Equals(y);
+            return x == y;
         }
+
         /// <inheritdoc/>
         public int GetHashCode(LokiLabel obj)
         {
             return obj.GetHashCode();
         }
+
         /// <inheritdoc/>
         public override bool Equals(object? obj)
         {
-            return obj is LokiLabel label && Equals(label);
+            return Equals(obj as LokiLabel);
         }
+
+
         /// <inheritdoc/>
-        public bool Equals(LokiLabel other)
+        public bool Equals(LokiLabel? other)
         {
-            return Key == other.Key &&
-                   Value == other.Value;
+            return Equals(this, other);
         }
+
         /// <inheritdoc/>
-        public static bool operator ==(LokiLabel left, LokiLabel right)
+        public static bool operator ==(LokiLabel? left, LokiLabel? right)
         {
-            return left.Equals(right);
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+
+            if (left is null || right is null)
+            {
+                return false;
+            }
+
+            return left.Key == right.Key && left.Value == right.Value;
         }
+
         /// <inheritdoc/>
-        public static bool operator !=(LokiLabel left, LokiLabel right)
+        public static bool operator !=(LokiLabel? left, LokiLabel? right)
         {
             return !(left == right);
         }
+
         /// <inheritdoc/>
         public override int GetHashCode()
         {
